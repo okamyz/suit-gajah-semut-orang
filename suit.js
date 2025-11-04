e34cda809a1e8b1caf69163605ef354a5cd9c20a
jaa <farezaahmadk@gmail.com>
2025-11-04 12:37:28 +0700

    file index

---
+++ suit.js
@@ -0,0 +1,40 @@
+var tanya = true;
+while (tanya) {
+// menangkap pilihan player
+    var p = prompt('pilih : gajah, semut, orang');
+
+    // menangkap pilihan computer
+    // membangkitkan bilangan random
+    var comp = Math.random();
+
+    if ( comp < 0.34 ) {
+        comp = 'gajah';
+    } else if( comp >= 0.34 && comp < 0.67) {
+        comp = 'orang';
+    } else {
+        comp = 'semut';
+    }
+
+
+    var hasil = '';
+    // menentukan rules
+    if ( p == comp) {
+        hasil = 'SERI!';
+    } else if ( p == 'gajah' ){
+        hasil = ( comp == 'orang' ) ? 'MENANG!' : 'KALAH!';
+    } else if ( p == 'orang' ){
+        hasil = ( comp == 'semut' ) ? 'MENANG!' : 'KALAH!';
+    } else if ( p == 'semut' ){
+        hasil = ( comp == 'gajah' ) ? 'MENANG!' : 'KALAH!';
+    } else {
+        hasil = 'Kamu memasukkan pilihan yang salah!!';
+    }
+
+
+    // tampilkan hasilnya
+    alert('Kamu memilih : ' + p + ' dan Komputer memilih : ' + comp + '\nMaka hasilnya : Kamu ' + hasil);
+
+    tanya = confirm('lagi?');
+}
+
+alert('terimakasih sudah bermain.');
